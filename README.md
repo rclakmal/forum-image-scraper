f68d389cb8ab5f8c8c42173ca39033e6d51288f0
Lakmal <rclakmal@gmail.com>
2025-11-09 22:39:19 +0100

    Simplied code, fix the issues with the parallel downloads

--- README.md
+++ README.md
@@ -1,58 +1,34 @@
 # Forum Image Scraper
-## Downloads images from forum threads
-##### Only works with forums which doesn't require a login to view and have an incremental pagination 
+
+Download images from paginated forum threads with parallel processing and duplicate detection.
+
+## Quick Start
+
+1. Install dependencies: `pip install -r requirements.txt`
+2. **Configure `input.properties`** - This file is required and contains all settings (forum URL, threads, page range, filters)
+3. Run: `python forum-image-scraper.py`
+
+## Output Structure
+
+Images are saved in a hierarchical folder structure matching the thread path:
+```
+../hostname/
+  └── path/
+      └── to/
+          └── thread/
+              ├── p1_abc12345.jpg
+              ├── p1_def67890.jpg
+              ├── p2_ghi11223.jpg
+              └── ...
+```
+
+- `hostname`: Extracted domain name from the forum
+- Full path hierarchy created from thread URL
+- `pX_hash.jpg`: Page number and unique hash identifier
 
 ## Features
-- Downloads all image references in a given forum thread. You can define the forum, the thread and required pages in the property file
-- Currently also supports linked FB/Insta images in the thread. Downloading FB/Insta images takes bit more time as we need to perform two calls to get the direct image link. You can opt out from downloading FB/Insta image links by setting "shouldDownloadSocialLinks" to False. 
 
-## How to use
-
-- Download the project as a zip file, unzip and move into the folder (it's around 11MB)
-- Locate the "input.properties" file and set properties. Here are some example property values
-    - hostSite = https://www.petforums.co.uk
-    - thread = https://www.petforums.co.uk/threads/cute-pet-photo-contest.1952
-    - pageAppenderBefore = /page-
-    - startPage = 1
-    - endPage = 20
-- "pageAppenderBefore" is different from forum to forum. You need to check this by visiting an individual page of the thread via the browser. This is the text between the "thread" url and page number. Few examples are below. 
-    - https://www.petforums.co.uk/threads/cute-pet-photo-contest.1952/page-/2 - , pageAppenderBefore =  /page-
-    - https://www.ar15.com/forums/general/Cat-meme-thread-because-why-the-hell-not-/5-2083401/&page=14 , pageAppenderBefore = /&page=
-- Verify "inputs.properties" and save.
-- Double click "forum-image-scraper.exe"
-- If double clicking doesn't work, open a command line prompt and run it by typing "forum-image-scraper.exe". This way you will be able to see if there are any issues with your property file. 
-- Images will be downloaded to a folder one directory up. Folder structure will be as follows.
-    - SiteName
-        - ThreadName
-            - Page
-
-## How to develop and package
-- "forum-image-scraper.py" contains the entire script. Pyinstaller is used to generate the "exe" file
-
-
-## What Next
-- Support more forum types
-
-## License
-
-MIT License
-
-Copyright (c) [2021] [[@CuriousYoda](https://twitter.com/CuriousYoda)]
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+- Parallel downloads (10 workers) for speed
+- Resolution filtering to skip small images
+- Hash-based duplicate detection across all pages
+- Progress feedback and statistics
--- forum-image-scraper.py
+++ forum-image-scraper.py
@@ -1,210 +1,214 @@
+﻿import os
 import re
-import sys
-import os
-import json
-import urllib
-from urllib.request import urlopen
-import requests
 import hashlib
-import zlib
 import configparser
+import requests
 from bs4 import BeautifulSoup
-from urllib.parse import unquote
-from urllib.parse import urlparse
+from urllib.parse import unquote, urljoin
+from PIL import Image
+from io import BytesIO
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import tldextract
 
-imgExtentions = ["png","jpeg","jpg","gif"]
+# Reusable session for connection pooling (faster downloads)
+session = requests.Session()
+session.headers.update({
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1'
+})
 
-config = configparser.RawConfigParser()
-config_file = open('input.properties', encoding="utf-8")
-config.read_file(config_file)
 
-# We read the user defined website from the properties file
-if config.get("UserInput", "hostSite"):
-    hostSite = config.get("UserInput", "hostSite")
-    print("HOST SITE: "+hostSite+"\n")
-else:
-    print("DEFINE HOST URL IN PROPERTY FILE")
-    exit
-
-# We read the user defined thread from the properties file
-if config.get("UserInput", "thread"):
-    threadUrl = config.get("UserInput", "thread")
-    simpliedThreadName = unquote(threadUrl.split("/")[-1])
-    print("DOWNLOADING THREAD: "+simpliedThreadName+"\n")
-else:
-    print("DEFINE A THREAD IN PROPERTY FILE")
-    exit
+def download_image(url, page, folder, url_hash, minWidth, minHeight):
+    """Download a single image. Returns (status, filename, message)."""
+    filename = f"p{page}_{url_hash}.jpg"
+    filepath = os.path.join(folder, filename)
     
-# We read the other properties
-pageAppenderBefore = config.get("UserInput", "pageAppenderBefore")
-pageAppenderAfter = config.get("UserInput", "pageAppenderAfter")
-page = int(config.get("UserInput", "startPage"))
-untilPage = int(config.get("UserInput", "endPage"))
-smallImageSize = int(config.get("UserInput", "smallImageSize"))
-pageValueMultiply = int(config.get("UserInput", "pageValueMultiply"))
+    try:
+        img_response = session.get(url, timeout=30)
+        if img_response.status_code == 200 and len(img_response.content) > 1000:
+            # Check image resolution
+            if minWidth > 0 or minHeight > 0:
+                try:
+                    img = Image.open(BytesIO(img_response.content))
+                    width, height = img.size
+                    if (minWidth > 0 and width < minWidth) or (minHeight > 0 and height < minHeight):
+                        return ('too_small', filename, f'{width}x{height}')
+                except:
+                    pass  # If can't read image, skip resolution check
+            
+            with open(filepath, 'wb') as f:
+                f.write(img_response.content)
+            return ('success', filename, len(img_response.content))
+        return ('failed', filename, 'Invalid response')
+    except Exception as e:
+        return ('error', filename, str(e))
 
-if config.get("UserInput", "shouldDownloadSocialLinks")== 'True':
-    shouldDownloadSocialLinks = True
-else:
-    shouldDownloadSocialLinks = False
 
-# We create a separate folder for each website/host
-websiteFolder = "../"+tldextract.extract(hostSite).domain
-if not os.path.isdir(websiteFolder):
-    os.mkdir(websiteFolder)
-
-# We create a separate folder for each thread
-threadFolder = websiteFolder+"/"+simpliedThreadName
-if not os.path.isdir(threadFolder):
-    os.mkdir(threadFolder)
-
-# Requests a url and returns the response   
-def requestUrl(url):
-    req = urllib.request.Request(
-                                    url, 
-                                    data=None, 
-                                    headers={
-                                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-                                    }
-                )
-    return urlopen(req)
-    
-# delete a file   
-def deleteFile(f):
-    f.close()
-    os.remove(f.name)
-
-# Create links for FB/Insta attached images
-def createSocialMediaUrl(responseText):
-    # Separate regex to find potential Insta photos
-    socialLinks = []
-    instaTags = re.findall("(instagram.min.html#[a-zA-Z]+)+", responseText);
-    for instaTag in instaTags:
-        instaUrl = "https://www.instagram.com/p/"+instaTag.split("#")[-1]+"/embed/"
-        socialLinks.append(instaUrl)
-    
-    # Separate regex to find potential FB photos
-    fbTags = re.findall("\d{6,9}_\d{15,17}_\d{16,19}_[o|n]", responseText);
-    for fbTag in fbTags:
-        fbUrl = "https://facebook.com/"+fbTag.split("_")[1]
-        socialLinks.append(fbUrl)
-        
-    return socialLinks
-
-# Generate FB direct image link and download the photo  
-def downloadFBPhoto(url):
-    fbResponse = requests.get(url)
-    soup = BeautifulSoup(fbResponse.text, 'html.parser') 
-    scriptTag =  soup.find('script', type='application/ld+json')
-    if scriptTag:
-        data = json.loads(scriptTag.string)
-        directFBImageLink = data['image']['contentUrl']
-        return requestUrl(directFBImageLink)
+def process_thread(hostSite, threadUrl, pageAppenderBefore, pageAppenderAfter, startPage, endPage, pageValueMultiply, minWidth, minHeight):
+    """Process a single thread."""
+    # Normalize thread URL - handle both full URLs and path-only URLs
+    if threadUrl.startswith('http://') or threadUrl.startswith('https://'):
+        # Full URL provided - extract the path for folder naming
+        full_thread_url = threadUrl.rstrip('/')
+        thread_path = threadUrl.split('://', 1)[1]  # Remove scheme
+        thread_path = thread_path.split('/', 1)[1] if '/' in thread_path else ''  # Remove domain
     else:
-        return fbResponse
+        # Path only - construct full URL using hostSite
+        full_thread_url = hostSite.rstrip('/') + '/' + threadUrl.lstrip('/')
+        thread_path = threadUrl.lstrip('/')
+    
+    # Create output folder structure: ../hostname/full/thread/path/
+    hostname = tldextract.extract(hostSite).domain
+    
+    # Use the full thread path to create folder hierarchy
+    # Clean each part of the path (remove special chars)
+    path_parts = [p for p in thread_path.rstrip('/').split('/') if p]
+    cleaned_parts = []
+    for part in path_parts:
+        # Decode URL encoding and clean special characters
+        cleaned = unquote(part)
+        cleaned = re.sub(r'[^\w\-]', '_', cleaned)
+        # Limit each part to reasonable length
+        cleaned = cleaned[:100]
+        cleaned_parts.append(cleaned)
+    
+    # Build full folder path: ../hostname/part1/part2/part3/
+    folder = os.path.join('..', hostname, *cleaned_parts)
+    os.makedirs(folder, exist_ok=True)
+
+    print(f"\n{'='*60}")
+    print(f"Thread: {thread_path}")
+    print(f"Pages {startPage} to {endPage}")
+    print(f"Output: {folder}")
+    print(f"{'='*60}\n")
+
+    # Track all downloaded hashes to avoid duplicates across pages
+    downloaded_hashes = set()
+    
+    # Check existing files and populate hash set
+    if os.path.exists(folder):
+        for existing_file in os.listdir(folder):
+            if existing_file.endswith('.jpg'):
+                # Extract hash from filename (format: pX_hash.jpg)
+                parts = existing_file.split('_')
+                if len(parts) == 2:
+                    hash_part = parts[1].replace('.jpg', '')
+                    downloaded_hashes.add(hash_part)
+
+    for page in range(startPage, endPage + 1):
+        print(f"\n{'─'*60}")
+        print(f"PAGE {page}")
         
-# Generate Insta direct image link and download the photo      
-def downloadInstaPhoto(url):
-    instaResponse = requestUrl(url).read()
-    soup = BeautifulSoup(instaResponse, 'html.parser')
-    img_url = soup.find_all('img')[1]['src']
-    if img_url:
-        return requestUrl(img_url)
-    else:
-        return instaResponse
-  
-# Modify Urls when Url is not complete. Different forums use different releative path formats. This method needs to be simplified.
-def modifyUrl(url):
-    if url.startswith("//"):
-        return '{}{}'.format("http:", url)
-    if url.startswith("/"):
-        return '{}{}'.format(hostSite, url)
-    elif url.startswith("http"):
-        return url;
-    elif url.find(".") > url.find("/"):
-        return '{}{}'.format(hostSite+"/", url)
-    else:
-        return '{}{}'.format("http://", url)
-              
-# We now iterate over pages
-while untilPage >= page:
-    print ("\n\nPARSING PAGE: "+str(page))
-
-    # We create a separate folder for each page
-    pageFolder = threadFolder+"/"+str(page)
-    if not os.path.isdir(pageFolder):
-        os.mkdir(pageFolder)
-    
-    # We download the page content and look for image references in the page
-    response = requests.get(threadUrl+pageAppenderBefore+str(page*pageValueMultiply)+pageAppenderAfter)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    img_tags = soup.find_all('img')
-    
-    # We also look for lazy loaded attachments and social media links
-    anchorTags = soup.findAll('a', attrs={'href': re.compile("(jpeg|jpg|gif|png|facebook|instagram)")})
- 
-    # We iterate over image and href references and generate a Url list
-    urls = []
-    for img in img_tags:
-        urls.append(modifyUrl(img['src']))
-    for anchor in anchorTags:
-        urls.append(modifyUrl(anchor['href']))
-    
-    # We do special processing for FB, Insta images to extract the proper direct link
-    if shouldDownloadSocialLinks:
-       socialLinks = createSocialMediaUrl(response.text)
-       urls  = urls + socialLinks
+        page_url = f"{full_thread_url}{pageAppenderBefore}{page * pageValueMultiply}{pageAppenderAfter}"
+        print(f"URL: {page_url}")
         
-    # We remove duplicates
-    urls = list(set(urls))
-    
-    print("PAGE PARSED SUCCESSFULLY")
-    print("DOWNLOADING IMAGES.....\n")
-
-    # We now iterate over all the unique Urls to download images. 
-    for url in urls:
-
-        # we provide a unique file name so that we don't save the file in our future executions of the script
-        filename = hashlib.md5(url.encode('utf-8')).hexdigest()+".jpg"
-        completeName = pageFolder+"/"+filename
-        if os.path.isfile(completeName):
-            print ("SKIPPED, ALREADY DOWNLOADED: "+url)
+        try:
+            response = session.get(page_url, timeout=30)
+            response.raise_for_status()
+        except Exception as e:
+            print(f"ERROR: {e}")
+            print(f"{'─'*60}")
             continue
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        urls = set()
         
-        with open(completeName, 'wb') as f:
-            try:
-                if shouldDownloadSocialLinks:
-                    if "facebook.com" in url:
-                        result = downloadFBPhoto(url)
-                    elif "instagram.com" in url:
-                        result = downloadInstaPhoto(url)
-                    else:
-                        result = requestUrl(url)
-                else:
-                    result = requestUrl(url)
-                    
-                if result.headers['content-type'] and not any(x in result.headers['content-type'] for x in imgExtentions):
-                    print ("SKIPPED, PROBABLY NOT A IMAGE: "+url)
-                    deleteFile(f)
-                    continue
-                    
-                if result.headers['content-length'] and int(result.headers['content-length']) < smallImageSize:
-                    print ("SKIPPED, TOO SMALL: "+url)
-                    deleteFile(f)
-                    continue
-                
-                response = result.read()
-                f.write(response)
-                print("\nSUCCESSFUL: "+url)
-                print("SAVED AS "+completeName+"\n")
-            except Exception as e:
-                print("\nERROR: "+url)
-                print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e,"\n")
-                deleteFile(f)
+        # Find all img tags
+        for img in soup.find_all('img'):
+            src = img.get('src')
+            if src and not src.startswith('data:'):  # Skip data URLs
+                url = urljoin(hostSite, src)
+                # Only include http/https URLs
+                if url.startswith(('http://', 'https://')):
+                    urls.add(url)
+        
+        # Find all links to images
+        for a in soup.find_all('a', href=re.compile(r'\.(jpg|jpeg|png|gif)$', re.I)):
+            href = a.get('href')
+            if href and not href.startswith('data:'):  # Skip data URLs
+                url = urljoin(hostSite, href)
+                if url.startswith(('http://', 'https://')):
+                    urls.add(url)
+
+        print(f"Found: {len(urls)} images")
+        downloaded = 0
+        skipped = 0
+        too_small = 0
+        
+        # Prepare download tasks
+        tasks = []
+        for url in urls:
+            url_hash = hashlib.md5(url.encode()).hexdigest()[:8]
+            
+            # Check if this hash was already downloaded (on any page)
+            if url_hash in downloaded_hashes:
+                skipped += 1
                 continue
+            
+            tasks.append((url, page, folder, url_hash, minWidth, minHeight))
+        
+        # Download images in parallel (max 10 concurrent downloads)
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = {executor.submit(download_image, *task): task for task in tasks}
+            
+            for future in as_completed(futures):
+                status, filename, info = future.result()
                 
-    # on to the next page            
-    page = page + 1;
+                if status == 'success':
+                    url_hash = filename.split('_')[1].replace('.jpg', '')
+                    downloaded_hashes.add(url_hash)
+                    downloaded += 1
+                    print(f"  ✓ {filename} ({info} bytes)")
+                elif status == 'too_small':
+                    too_small += 1
+                elif status == 'error':
+                    print(f"  ✗ {filename} - {info}")
+
+        print(f"\nSummary:")
+        if downloaded > 0:
+            print(f"  Downloaded: {downloaded}")
+        if skipped > 0:
+            print(f"  Skipped (already have): {skipped}")
+        if too_small > 0:
+            print(f"  Skipped (too small): {too_small}")
+        print(f"{'─'*60}")
+
+
+def main():
+    config = configparser.RawConfigParser()
+    with open('input.properties', encoding="utf-8") as f:
+        config.read_file(f)
+
+    hostSite = config.get("UserInput", "hostSite")
+    threads_raw = config.get("UserInput", "thread")
+    pageAppenderBefore = config.get("UserInput", "pageAppenderBefore")
+    pageAppenderAfter = config.get("UserInput", "pageAppenderAfter", fallback="")
+    startPage = int(config.get("UserInput", "startPage"))
+    endPage = int(config.get("UserInput", "endPage"))
+    pageValueMultiply = int(config.get("UserInput", "pageValueMultiply", fallback="1"))
+    minWidth = int(config.get("UserInput", "minWidth", fallback="0"))
+    minHeight = int(config.get("UserInput", "minHeight", fallback="0"))
+
+    # Parse comma-separated threads
+    thread_urls = [t.strip() for t in threads_raw.split(',') if t.strip()]
     
-   
+    print(f"Forum: {hostSite}")
+    print(f"Processing {len(thread_urls)} thread(s)")
+    
+    # Process each thread
+    for threadUrl in thread_urls:
+        process_thread(hostSite, threadUrl, pageAppenderBefore, pageAppenderAfter, 
+                      startPage, endPage, pageValueMultiply, minWidth, minHeight)
+    
+    print(f"\n{'='*60}")
+    print("All threads completed!")
+    print(f"{'='*60}\n")
+
+
+if __name__ == '__main__':
+    main()
